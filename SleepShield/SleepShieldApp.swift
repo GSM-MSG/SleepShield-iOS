43f6727277a936f08ac5587f1903048460d080e3
baegteun <baegteun@gmail.com>
2025-09-20 23:07:50 +0900

    Initial Commit

---
+++ SleepShield/SleepShieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepShieldApp.swift
+//  SleepShield
+//
+//  Created by baegteun on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SleepShieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SleepShieldTests/SleepShieldTests.swift
@@ -0,0 +1,17 @@
+//
+//  SleepShieldTests.swift
+//  SleepShieldTests
+//
+//  Created by baegteun on 9/20/25.
+//
+
+import Testing
+@testable import SleepShield
+
+struct SleepShieldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
